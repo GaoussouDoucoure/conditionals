1ded16dd5737664366f31b033f3dab22b92083d3
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-10 10:01:53 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void go (int age){
+        if(age >= 18){
+            System.out.println("Go VOTE!!!");
+        }else{
+            System.out.println("Go PLAY!!!");
         }
+    }
+
+    public static void respond (char answer){
+        if(answer == 'Y' || answer == 'y'){
+            System.out.println("YUP YUP YUP");
+        }else{
+            System.out.println("NOPE NOPE NOPE");
+        }
+    }
+
+    public static void drink(int age){
+        if(age < 21){
+            System.out.println("juice");
+        }else if(age < 50){
+            System.out.println("adult beverage");
+        }else if(age < 77) {
+            System.out.println("tea");
+        }else{
+            System.out.println("heavy beverage");
+        }
+    }
+
+    public static void manUtd(String result){
+        if(result == "W" || result == "w"){
+            System.out.println("ManUtd is the best team in the world!!!");
+        }else if(result == "D" || result == "d"){
+            System.out.println("ManUtd was unlucky :(");
+        }else{
+            System.out.println("This team sucks, I'm done supporting it! till next game :)");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        manUtd("W");
+        manUtd("D");
+        manUtd("L");
+
+        drink(11); // juice
+        drink(25); // adult beverage
+        drink(70); // tea
+        drink(99); // heavy beverage
+
+       /* for(String a : args){
+            System.out.println(a);
+        }*/
+
+        go(13);
+        go(26);
+
+        respond('y');
+        respond('n');
     }
 }
